958e6dea83eab32b76f94e66b9531d40f78f94e7
Pidugu Sai Kiran <Pidugu.SaiKiran@evernorth.com>
2025-04-10 15:04:31 +0530

    preview chart fix

--- src/app/components/chart-preview/chart-preview.component.ts
+++ src/app/components/chart-preview/chart-preview.component.ts
@@ -3,31 +3,35 @@ import { Component, Input, OnChanges } from '@angular/core';
 // import * as Charts from 'fusioncharts/fusioncharts.charts';
 // import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 import { ChartConfig } from '../../models/chart.model';
-import { FusionChartsModule } from 'angular-fusioncharts';
+// import { FusionChartsModule } from 'angular-fusioncharts';
 
-import * as FusionCharts from 'fusioncharts';
-import * as Charts from 'fusioncharts/fusioncharts.charts';
-import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+// import * as FusionCharts from 'fusioncharts';
+// import * as Charts from 'fusioncharts/fusioncharts.charts';
+// import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
-// Initialize FusionCharts Module
-const fc = FusionCharts.default || FusionCharts;
-FusionChartsModule.fcRoot(fc, Charts, FusionTheme);
+// // Initialize FusionCharts Module
+// const fc = FusionCharts.default || FusionCharts;
+// FusionChartsModule.fcRoot(fc, Charts, FusionTheme);
+
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+Charts(FusionCharts);
 
 
 @Component({
   selector: 'app-chart-preview',
   standalone: true,
-  imports: [FusionChartsModule],
+  imports: [],
   template: `
-    <!-- <div class="chart-container" id="testing-chart"></div> -->
+    <div class="chart-container" id="testing-chart"></div>
     <!-- @if(!isChartLoading) { -->
-    <fusioncharts
+    <!-- <fusioncharts
       [width]="config.width"
       [height]="config.height"
       [type]="config.type"
       [dataFormat]="config.dataFormat"
       [dataSource]="config.dataSource"
-    ></fusioncharts>
+    ></fusioncharts> -->
     <!-- } -->
   `
 })
@@ -42,40 +46,40 @@ export class ChartPreviewComponent {
   ngOnChanges(): void {
     this.isChartLoading = true;
     if (this.config && this.chartType) {
-      // this.renderChart();
-      setTimeout(() => {
-        this.isChartLoading = false;
-      }, 300);
+      this.renderChart();
+      // setTimeout(() => {
+      //   this.isChartLoading = false;
+      // }, 300);
     }
   }
 
-  // private renderChart(): void {
-  //   if (this.chart) {
-  //     this.chart.dispose();
-  //   }
+  private renderChart(): void {
+    if (this.chart) {
+      this.chart.dispose();
+    }
 
-  //   this.chartId = this.chartId;
+    this.chartId = this.chartId;
 
-  //   const chartConfig: any = {
-  //     ...this.config,
-  //     type: this.chartType,
-  //     // width: '100%',
-  //     // height: '100%',
-  //     // dataFormat: 'json',
-  //     renderAt: 'testing-chart', // this.chartId
-  //   };
+    const chartConfig: any = {
+      ...this.config,
+      type: this.chartType,
+      // width: '100%',
+      // height: '100%',
+      // dataFormat: 'json',
+      renderAt: 'testing-chart', // this.chartId
+    };
 
-  //   try {
-  //     this.chart = new FusionCharts(chartConfig);
-  //     this.chart.render();
-  //   } catch (error) {
-  //     console.error('Error rendering chart:', error);
-  //   }
-  // }
+    try {
+      this.chart = new FusionCharts(chartConfig);
+      this.chart.render();
+    } catch (error) {
+      console.error('Error rendering chart:', error);
+    }
+  }
 
-  // ngOnDestroy(): void {
-  //   if (this.chart) {
-  //     this.chart.dispose();
-  //   }
-  // }
+  ngOnDestroy(): void {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
 }
